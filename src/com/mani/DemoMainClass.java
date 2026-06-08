9f8fbef32e9ab71277e7674c085ebd9673d6540c
Manikanth8447 <manikanthpusalwar11@gmail.com>
2026-06-08 14:58:38 +0530

    Practicing git workflow

--- src/com/mani/DemoMainClass.java
+++ src/com/mani/DemoMainClass.java
@@ -5,9 +5,9 @@ public class DemoMainClass {
 	public static void main(String[] args) {
 
 		System.out.println("DemoMainClass.main()");
-		System.out.println("DemoMainClass.main()");
-		System.out.println("DemoMainClass.main()");
-		System.out.println("DemoMainClass.main()");
+		
+		System.out.println("Git Practice");
+		System.out.println("Git Practice");
 	}
 
 }
