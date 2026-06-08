4cbe95672f81322f528e82a706f80b95f79c68d9
Manikanth8447 <manikanthpusalwar11@gmail.com>
2026-06-08 14:38:09 +0530

    2nd commit

--- src/com/mani/DemoMainClass.java
+++ src/com/mani/DemoMainClass.java
@@ -5,6 +5,9 @@ public class DemoMainClass {
 	public static void main(String[] args) {
 
 		System.out.println("DemoMainClass.main()");
+		System.out.println("DemoMainClass.main()");
+		System.out.println("DemoMainClass.main()");
+		System.out.println("DemoMainClass.main()");
 	}
 
 }
